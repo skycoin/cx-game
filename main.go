3e9033b37700cc1be052173c996fa056a0f0d729
cbrom abody <kb.cbrom@gmail.com>
2021-03-23 09:47:13 +0300

    glfw window render and update scaffolds

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"image"
+	"image/png"
+	"log"
+	"os"
 	"runtime"
-	"time"
 
+	"github.com/go-gl/gl/v2.1/gl"
 	"github.com/go-gl/glfw/v3.3/glfw"
-
+	"github.com/skycoin/cx-game/render"
 	"github.com/skycoin/cx-game/world"
 )
 
@@ -16,35 +20,28 @@ func init() {
 }
 
 var (
-	FPS int
+	DrawCollisionBoxes = false
+	FPS                int
 )
 
 var CurrentPlanet *world.Planet
 
+const (
+	width  = 800
+	height = 480
+)
+
 func main() {
-	err := glfw.Init()
-	if err != nil {
-		panic(err)
-	}
+
+	win := render.NewWindow(height, width, true)
+	window := win.Window
 	defer glfw.Terminate()
 
-	window, err := glfw.CreateWindow(640, 480, "Testing", nil, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	window.MakeContextCurrent()
+	program := win.Program
 
 	for !window.ShouldClose() {
-		// Do OpenGL stuff.
-		window.SwapBuffers()
-		glfw.PollEvents()
-	}
-
-	for {
 		Tick()
-		time.Sleep(100 * time.Millisecond)
-		Draw()
+		draw(window, program)
 	}
 }
 
@@ -52,6 +49,40 @@ func Tick() {
 
 }
 
-func Draw() {
+func draw(window *glfw.Window, program uint32) {
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+	gl.UseProgram(program)
 
+	imgFile, err := os.Open("./assets/Daco_3555790.png")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer imgFile.Close()
+
+	// Decode detexts the type of image as long as its image/<type> is imported
+	im, err := png.Decode(imgFile)
+
+	img := im.(*image.NRGBA)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	size := img.Rect.Size()
+
+	// flip image: first pixel is lower left corner
+	data := make([]byte, size.X*size.Y*4)
+	lineLen := size.X * 4
+	dest := len(data) - lineLen
+	for src := 0; src < len(img.Pix); src += img.Stride {
+		copy(data[dest:dest+lineLen], img.Pix[src:src+img.Stride])
+		dest -= lineLen
+	}
+
+	gl.TexImage2D(
+		gl.TEXTURE_2D, 0, gl.RGBA, int32(size.X), int32(size.Y),
+		0, gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(data))
+
+	glfw.PollEvents()
+	window.SwapBuffers()
 }
