8a4b3ca34b19dcf6b3bd373c03313d24f782e14d
tonytomato <antonio.tomatinio@gmail.com>
2021-04-11 22:57:47 +0300

    Sprite tool update - auto contour simplification algorithm update - add contour point tool - erase contour point tool - add joint tool

--- cmd/spritetool/contours.go
+++ cmd/spritetool/contours.go
@@ -1,10 +1,12 @@
 package cv
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"image/draw"
-	"image/jpeg"
+	"image/png"
+	"sort"
 
 	"github.com/go-gl/gl/v4.1-core/gl"
 	"github.com/go-gl/glfw/v3.3/glfw"
@@ -68,10 +70,45 @@ func (n *Node) reset() {
 	n.next_sibling = -1
 }
 
+type Joint struct {
+	parentIndex int
+	pivot       Point
+	parentPivot Point
+}
+
 type Contour struct {
-	points       []Point
-	boundingRect Rect
-	centerOfMass Point
+	points        []Point
+	wrapPoints    []Point
+	boundingRect  Rect
+	centerOfMass  Point
+	name          string
+	img           image.RGBA
+	selectedIndex int
+	joint         Joint
+}
+
+func PerpendicularDistance(pt, lineStart, lineEnd Point) float32 {
+
+	dx := float64(lineEnd.x - lineStart.x)
+	dy := float64(lineEnd.y - lineStart.y)
+	mag := math.Sqrt(math.Pow(dx, 2) + math.Pow(dy, 2))
+	if mag > 0 {
+		dx /= mag
+		dy /= mag
+	}
+
+	pvx := float64(pt.x - lineStart.x)
+	pvy := float64(pt.y - lineStart.y)
+
+	pvdot := dx*pvx + dy*pvy
+
+	dsx := pvdot * dx
+	dsy := pvdot * dy
+
+	ax := pvx - dsx
+	ay := pvy - dsy
+
+	return float32(math.Sqrt(math.Pow(ax, 2) + math.Pow(ay, 2)))
 }
 
 func stepCCW(current *Point, pivot Point) {
@@ -247,10 +284,10 @@ func BoundingRect(contour []Point) Rect {
 		}
 	}
 	var ret Rect
-	ret.pos.x = min.x
-	ret.pos.y = min.y
-	ret.width = max.x - min.x
-	ret.height = max.y - min.y
+	ret.pos.x = min.x - 1
+	ret.pos.y = min.y - 1
+	ret.width = max.x - min.x + 2
+	ret.height = max.y - min.y + 2
 	return ret
 }
 
@@ -362,6 +399,18 @@ func drawRectangle(r Rect, img *image.RGBA, col color.Color) {
 	drawLine(NewPoint(x2, y1), NewPoint(x2, y2), img, col)
 }
 
+func drawPoint(p Point, img *image.RGBA, col color.Color) {
+	x1 := p.x - 5
+	y1 := p.y - 5
+	x2 := p.x + 5
+	y2 := p.y + 5
+	for x := x1; x <= x2; x++ {
+		for y := y1; y <= y2; y++ {
+			img.Set(x, y, col)
+		}
+	}
+}
+
 func (r *Rect) Contains(r2 Rect) bool {
 	return r2.pos.x > r.pos.x && r2.pos.y > r.pos.y && r2.pos.x+r2.width < r.pos.x+r.width && r2.pos.y+r2.height < r.pos.y+r.height
 }
@@ -377,12 +426,101 @@ func (c *Contour) CalculateCenterOfMass() {
 	c.centerOfMass = tmpCenter
 }
 
+type Queue struct {
+	elements chan Point
+}
+
+func NewQueue(size int) *Queue {
+	return &Queue{
+		elements: make(chan Point, size),
+	}
+}
+
+func (queue *Queue) Push(element Point) {
+	select {
+	case queue.elements <- element:
+	default:
+		panic("Queue full")
+	}
+}
+
+func (queue *Queue) Pop(p *Point) bool {
+	select {
+	case *p = <-queue.elements:
+		return true
+	default:
+		return false
+	}
+}
+
+func MaskFromContour(contour []Point, width, height int, offset Point) [][]int {
+	var ret [][]int = make([][]int, width)
+
+	for i := 0; i < width; i++ {
+		ret[i] = make([]int, height)
+	}
+	for i := range contour {
+		ret[contour[i].x-offset.x][contour[i].y-offset.y] = 1
+	}
+
+	//	var stack []Point
+	var stack = NewQueue(100)
+	var spanAbove bool
+	var spanBelow bool
+	stack.Push(NewPoint(0, 0))
+	//	stack.Push(NewPoint(width-1, 0))
+	//	stack.Push(NewPoint(width-1, height-2))
+	//	stack.Push(NewPoint(0, height-2))
+	var p Point
+	for stack.Pop(&p) {
+		x1 := p.x
+		for x1 >= 0 && ret[x1][p.y] == 0 {
+			x1--
+		}
+		x1++
+		spanAbove = false
+		spanBelow = false
+		for x1 < width && ret[x1][p.y] == 0 {
+			ret[x1][p.y] = 1
+			if !spanAbove && p.y > 0 && ret[x1][p.y] == 0 {
+				stack.Push(NewPoint(x1, p.y-1))
+				spanAbove = true
+			} else if spanAbove && p.y > 0 && ret[x1][p.y-1] != 0 {
+				spanAbove = false
+			}
+			if !spanBelow && p.y < height-1 && ret[x1][p.y+1] == 0 {
+				stack.Push(NewPoint(x1, p.y+1))
+				spanBelow = true
+			} else if spanBelow && p.y < height-1 && ret[x1][p.y+1] != 0 {
+				spanBelow = false
+			}
+			x1++
+		}
+	}
+	return ret
+}
+
+const (
+	JointTool = iota
+	AddPointTool
+	RemovePointTool
+	SelectTool
+
+//    West
+)
+
 type SpriteSet struct {
 	Contours []Contour
 	img      image.Image
 	binImage [][]int
-	epsilon  float32
-	drawtype int
+	mousePos Point
+
+	selectedContour int
+	tool            int
+	drawType        int
+	canDrag         bool
+	dragOffset      Point
+	tmpJoint        Joint
 }
 
 func (ss *SpriteSet) LoadFile(filename string, threshold int, binanirizeInverted bool) {
@@ -391,7 +529,7 @@ func (ss *SpriteSet) LoadFile(filename string, threshold int, binanirizeInverted
 		log.Fatalln(err)
 	}
 	defer imgFile.Close()
-	ss.img, err = jpeg.Decode(imgFile)
+	ss.img, err = png.Decode(imgFile)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -468,6 +606,7 @@ func (ss *SpriteSet) ProcessContours() {
 		var tmpContour Contour
 		tmpContour.boundingRect = BoundingRect(contours[i])
 		tmpContour.points = contours[i]
+		tmpContour.selectedIndex = -1
 		tmpContours = append(tmpContours, tmpContour)
 	}
 	for i := range tmpContours {
@@ -477,11 +616,36 @@ func (ss *SpriteSet) ProcessContours() {
 				contains = true
 			}
 		}
-		if !contains {
+		if !contains && tmpContours[i].boundingRect.height > 0 && tmpContours[i].boundingRect.width > 0 {
+			tmpContours[i].ConvexHull()
 			ss.Contours = append(ss.Contours, tmpContours[i])
 		}
 	}
-
+	for i := range ss.Contours {
+		imrect := image.Rect(0, 0, ss.Contours[i].boundingRect.width, ss.Contours[i].boundingRect.height)
+		brect := image.Rect(ss.Contours[i].boundingRect.pos.x, ss.Contours[i].boundingRect.pos.y, ss.Contours[i].boundingRect.pos.x+ss.Contours[i].boundingRect.width, ss.Contours[i].boundingRect.pos.y+ss.Contours[i].boundingRect.height)
+		ss.Contours[i].img = *image.NewRGBA(imrect)
+		draw.Draw(&ss.Contours[i].img, imrect, ss.img, brect.Min, draw.Src)
+		mask := MaskFromContour(ss.Contours[i].points, ss.Contours[i].boundingRect.width+1, ss.Contours[i].boundingRect.height+1, ss.Contours[i].boundingRect.pos)
+		for y := 0; y < ss.Contours[i].boundingRect.height; y++ {
+			for x := 0; x < ss.Contours[i].boundingRect.width; x++ {
+				if mask[x][y] == 1 {
+					ss.Contours[i].img.Set(x, y, color.Transparent)
+				}
+			}
+		}
+		ss.Contours[i].joint.parentIndex = -1
+		fn := fmt.Sprintf("c:\\test\\%d_test.png", i+1)
+		imgFile, err := os.Create(fn)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		defer imgFile.Close()
+		err = png.Encode(imgFile, &ss.Contours[i].img)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
 }
 
 var (
@@ -514,51 +678,321 @@ func makeVao() uint32 {
 	return vao
 }
 
+func euclidianDistance(p1, p2 Point) float64 {
+	return math.Sqrt(math.Pow(float64(p1.x-p2.x), 2) + math.Pow(float64(p1.y-p2.y), 2))
+}
+
+func (ss *SpriteSet) cursorPosCallback(w *glfw.Window, xpos float64, ypos float64) {
+	ss.mousePos.x = int(xpos)
+	ss.mousePos.y = int(ypos)
+	if ss.canDrag {
+		ss.Contours[ss.selectedContour].wrapPoints[ss.Contours[ss.selectedContour].selectedIndex] = NewPoint(ss.mousePos.x+ss.dragOffset.x, ss.mousePos.y+ss.dragOffset.y)
+	}
+}
+
+func (ss *SpriteSet) addPoint(p Point) {
+	ss.Contours[ss.selectedContour].wrapPoints = append(ss.Contours[ss.selectedContour].wrapPoints, p)
+	sort.Slice(ss.Contours[ss.selectedContour].wrapPoints, func(i int, j int) bool {
+		tmpvp1 := NewPoint(ss.Contours[ss.selectedContour].wrapPoints[i].x-ss.Contours[ss.selectedContour].centerOfMass.x, ss.Contours[ss.selectedContour].wrapPoints[i].y-ss.Contours[ss.selectedContour].centerOfMass.y)
+		tmpan1 := math.Atan2(float64(tmpvp1.x), float64(tmpvp1.y))
+		tmpvp2 := NewPoint(ss.Contours[ss.selectedContour].wrapPoints[j].x-ss.Contours[ss.selectedContour].centerOfMass.x, ss.Contours[ss.selectedContour].wrapPoints[j].y-ss.Contours[ss.selectedContour].centerOfMass.y)
+		tmpan2 := math.Atan2(float64(tmpvp2.x), float64(tmpvp2.y))
+		return tmpan1 < tmpan2
+	})
+}
+
+func (r *Rect) isPointInside(p Point) bool {
+	return p.x >= r.pos.x && p.y >= r.pos.y && p.x <= r.pos.x+r.width && p.y <= r.pos.y+r.height
+}
+
+func (ss *SpriteSet) mouseButtonCallback(w *glfw.Window, button glfw.MouseButton, action glfw.Action, mod glfw.ModifierKey) {
+	if action == glfw.Press {
+		if ss.tool == SelectTool {
+			ss.selectClosestPoint(ss.mousePos)
+		}
+		if ss.tool == AddPointTool {
+			ss.addPoint(ss.mousePos)
+		}
+		if ss.tool == JointTool {
+			ss.AddJoint()
+		}
+	}
+	if action == glfw.Release {
+		ss.canDrag = false
+	}
+}
+
+func (ss *SpriteSet) AddJoint() {
+	ss.selectContour(ss.mousePos)
+	if ss.selectedContour != -1 {
+		if ss.tmpJoint.parentIndex == -1 {
+			ss.tmpJoint.parentIndex = ss.selectedContour
+			ss.tmpJoint.parentPivot = NewPoint(ss.mousePos.x-ss.Contours[ss.selectedContour].boundingRect.pos.x, ss.mousePos.y-ss.Contours[ss.selectedContour].boundingRect.pos.y)
+		} else {
+			ss.tmpJoint.pivot = NewPoint(ss.mousePos.x-ss.Contours[ss.selectedContour].boundingRect.pos.x, ss.mousePos.y-ss.Contours[ss.selectedContour].boundingRect.pos.y)
+			ss.Contours[ss.selectedContour].joint = ss.tmpJoint
+			ss.tmpJoint.parentIndex = -1
+		}
+	}
+}
+
 func (ss *SpriteSet) keyCallBack(w *glfw.Window, k glfw.Key, s int, a glfw.Action, mk glfw.ModifierKey) {
 	if a == glfw.Press {
 		if k == glfw.KeyEscape {
 			w.SetShouldClose(true)
 		}
-		if k == glfw.KeyUp {
-			ss.epsilon += 1
+		if k == glfw.KeyJ {
+			w.SetTitle("Joint tool. Create pivot point on parent")
+			ss.tool = JointTool
+			ss.drawType = 2
+			ss.tmpJoint.parentIndex = -1
 		}
-		if k == glfw.KeyDown {
-			if ss.epsilon > 0 {
-				ss.epsilon -= 1
+		if k == glfw.KeyA {
+			w.SetTitle("Add point tool")
+			ss.tool = AddPointTool
+			ss.drawType = 1
+		}
+		if k == glfw.KeyS {
+			w.SetTitle("Select tool")
+			ss.tool = SelectTool
+			ss.drawType = 1
+		}
+		if k == glfw.KeyD {
+			ss.drawType = 1 - ss.drawType
+		}
+		if k == glfw.KeyE {
+			ss.eraseSelectedPoint()
+			ss.drawType = 1
+		}
+	}
+}
+
+func orient(a, b, c Point) int {
+	v := (b.y-a.y)*(c.x-b.x) - (b.x-a.x)*(c.y-b.y)
+	if v == 0 {
+		return 0
+	} else if v > 0 {
+		return 1
+	} else {
+		return 2
+	}
+}
+
+func (c *Contour) ConvexHull() {
+	m := len(c.points)
+	if m < 3 {
+		return
+	}
+	l := 0
+
+	var next []int
+	for i := 0; i < m; i++ {
+		next = append(next, -1)
+	}
+
+	for i := 1; i < m; i++ {
+		if c.points[i].x < c.points[l].x {
+			l = i
+		}
+	}
+	p := l
+	var q int
+	for {
+		q = (p + 1) % m
+		for i := 0; i < m; i++ {
+			if orient(c.points[p], c.points[i], c.points[q]) == 2 {
+				q = i
 			}
 		}
-		if k == glfw.KeySpace {
-			ss.epsilon = 0
+		next[p] = q
+		p = q
+		if p == l {
+			break
+		}
+	}
+	for i := 0; i < m; i++ {
+		if next[i] != -1 {
+			c.wrapPoints = append(c.wrapPoints, c.points[i])
+		}
+	}
+
+}
+
+func drawContour(points []Point, img *image.RGBA, col color.Color, selectedPoint int, selectedPointColor color.Color) {
+	if len(points) > 1 {
+		for j := 1; j < len(points); j++ {
+			drawLine(points[j], points[j-1], img, col)
+		}
+		drawLine(points[0], points[len(points)-1], img, col)
+		for j := range points {
+			if j == selectedPoint {
+				drawPoint(points[j], img, selectedPointColor)
+			} else {
+				drawPoint(points[j], img, col)
+			}
+		}
+	}
+}
+
+/*func drawContourDeb(points []Point, img *image.RGBA, col color.Color, selectedPoint int, selectedPointColor color.Color, f int, s int) {
+	if len(points) > 1 {
+		for j := 1; j < len(points); j++ {
+			drawLine(points[j], points[j-1], img, col)
+		}
+		d := color.RGBA{255, 255, 0, 255}
+		drawLine(points[0], points[len(points)-1], img, col)
+		for j := range points {
+			if j == selectedPoint {
+				drawPoint(points[j], img, selectedPointColor)
+			} else if j == f {
+				drawPoint(points[j], img, d)
+			} else if j == s {
+				drawPoint(points[j], img, d)
+			} else {
+				drawPoint(points[j], img, col)
+			}
+		}
+	}
+}
+*/
+func (ss *SpriteSet) eraseSelectedPoint() {
+	if ss.Contours[ss.selectedContour].selectedIndex > -1 {
+		copy(ss.Contours[ss.selectedContour].wrapPoints[ss.Contours[ss.selectedContour].selectedIndex:], ss.Contours[ss.selectedContour].wrapPoints[ss.Contours[ss.selectedContour].selectedIndex+1:])
+		ss.Contours[ss.selectedContour].wrapPoints[len(ss.Contours[ss.selectedContour].wrapPoints)-1] = NewPoint(0, 0)
+		ss.Contours[ss.selectedContour].wrapPoints = ss.Contours[ss.selectedContour].wrapPoints[:len(ss.Contours[ss.selectedContour].wrapPoints)-1]
+	}
+}
+
+func (ss *SpriteSet) selectContour(p Point) {
+	ss.selectedContour = -1
+	for i := range ss.Contours {
+		if ss.Contours[i].boundingRect.isPointInside(p) {
+			ss.selectedContour = i
+		}
+	}
+}
+
+func (ss *SpriteSet) selectClosestPoint(p Point) {
+	selectedPoint := -1
+	for i := range ss.Contours {
+		//		if ss.Contours[i].boundingRect.isPointInside(p) {
+		for j := range ss.Contours[i].wrapPoints {
+			dist := euclidianDistance(ss.Contours[i].wrapPoints[j], p)
+			if dist < 7 {
+				selectedPoint = j
+				ss.selectedContour = i
+			}
+		}
+		//		}
+	}
+	if selectedPoint > -1 {
+		ss.Contours[ss.selectedContour].selectedIndex = selectedPoint
+		ss.dragOffset = NewPoint(ss.Contours[ss.selectedContour].wrapPoints[selectedPoint].x-ss.mousePos.x, ss.Contours[ss.selectedContour].wrapPoints[selectedPoint].y-ss.mousePos.y)
+		ss.canDrag = true
+	} else {
+		selected := -1
+		for c := range ss.Contours {
+			if ss.Contours[c].boundingRect.isPointInside(p) {
+				selected = c
+			}
+		}
+		if selected > -1 {
+			ss.selectedContour = selected
+			ss.Contours[ss.selectedContour].selectedIndex = -1
+		}
+	}
+}
+
+func isAllDrawn(drawnPoints []Point) bool {
+	for i := range drawnPoints {
+		if drawnPoints[i].x == -1 && drawnPoints[i].y == -1 {
+			return false
+		}
+	}
+	return true
+}
+
+func (ss *SpriteSet) DrawJoints(img *image.RGBA) {
+	drawnPositions := make([]Point, len(ss.Contours))
+	width := ss.img.Bounds().Max.X
+	//height = ss.img.Bounds().Max.Y
+	for i := range drawnPositions {
+		drawnPositions[i] = NewPoint(-1, -1)
+	}
+	//first we draw all independent contours
+	for i := range ss.Contours {
+		if ss.Contours[i].joint.parentIndex == -1 {
+			p := NewPoint(ss.Contours[i].boundingRect.pos.x+width, ss.Contours[i].boundingRect.pos.y)
+			imrect := image.Rect(p.x, p.y, p.x+ss.Contours[i].img.Bounds().Max.X, p.y+ss.Contours[i].img.Bounds().Max.Y)
+			draw.Draw(img, imrect, &ss.Contours[i].img, image.Pt(0, 0), draw.Over)
+			drawnPositions[i] = p
+		}
+	}
+	for !isAllDrawn(drawnPositions) {
+		for i := range ss.Contours {
+			if ss.Contours[i].joint.parentIndex != -1 {
+				if drawnPositions[ss.Contours[i].joint.parentIndex].x != -1 && drawnPositions[i].x == -1 {
+					x := drawnPositions[ss.Contours[i].joint.parentIndex].x + ss.Contours[i].joint.parentPivot.x - ss.Contours[i].joint.pivot.x
+					y := drawnPositions[ss.Contours[i].joint.parentIndex].y + ss.Contours[i].joint.parentPivot.y - ss.Contours[i].joint.pivot.y
+					imrect := image.Rect(x, y, x+ss.Contours[i].img.Bounds().Max.X, y+ss.Contours[i].img.Bounds().Max.Y)
+					draw.Draw(img, imrect, &ss.Contours[i].img, image.Pt(0, 0), draw.Over)
+					drawnPositions[i] = NewPoint(x, y)
+				}
+			}
 		}
 	}
 }
 
 func (ss *SpriteSet) DrawSprite() {
 	var tex uint32
-
+	ss.drawType = 2
 	width := ss.img.Bounds().Max.X
 	height := ss.img.Bounds().Max.Y
 
-	win := render.NewWindow(height, width, true)
+	win := render.NewWindow(height, width*2, true)
 	window := win.Window
 	defer glfw.Terminate()
 	window.SetKeyCallback(ss.keyCallBack)
+	window.SetMouseButtonCallback(ss.mouseButtonCallback)
+	window.SetCursorPosCallback(ss.cursorPosCallback)
 	VAO := makeVao()
 	program := win.Program
 	gl.GenTextures(1, &tex)
 	for !window.ShouldClose() {
-		img := image.NewRGBA(ss.img.Bounds())
-		draw.Draw(img, img.Bounds(), ss.img, image.Pt(0, 0), draw.Src)
+		imgRect := image.Rect(0, 0, width*2, height)
+		img := image.NewRGBA(imgRect)
+		for x := 0; x < imgRect.Max.X; x++ {
+			for y := 0; y < imgRect.Max.Y; y++ {
+				img.Set(x, y, color.White)
+			}
+		}
+		imr1 := image.Rect(0, 0, ss.img.Bounds().Max.X, ss.img.Bounds().Max.Y)
+		draw.Draw(img, imr1, ss.img, image.Pt(0, 0), draw.Src)
+
 		red := color.RGBA{255, 0, 0, 255}
+		green := color.RGBA{0, 255, 0, 255}
+		blue := color.RGBA{0, 0, 255, 255}
+
 		for i := range ss.Contours {
-			if ss.epsilon == 0 {
-				drawRectangle(ss.Contours[i].boundingRect, img, red)
-			} else {
-				tmppnt := RamerDouglasPeucker(ss.Contours[i].points, ss.epsilon)
-				for j := 1; j < len(tmppnt); j++ {
-					drawLine(tmppnt[j], tmppnt[j-1], img, red)
+			if ss.drawType == 0 {
+				if ss.selectedContour == i {
+					drawRectangle(ss.Contours[i].boundingRect, img, green)
+				} else {
+					drawRectangle(ss.Contours[i].boundingRect, img, red)
 				}
-				drawLine(tmppnt[0], tmppnt[len(tmppnt)-1], img, red)
+			} else if ss.drawType == 1 {
+				if ss.selectedContour == i {
+					drawContour(ss.Contours[i].wrapPoints, img, green, ss.Contours[i].selectedIndex, blue)
+				} else {
+					drawContour(ss.Contours[i].wrapPoints, img, red, ss.Contours[i].selectedIndex, blue)
+				}
+			} else if ss.drawType == 2 {
+				if ss.selectedContour == i {
+					drawRectangle(ss.Contours[i].boundingRect, img, green)
+				} else {
+					drawRectangle(ss.Contours[i].boundingRect, img, red)
+				}
+				ss.DrawJoints(img)
 			}
 			ss.Contours[i].CalculateCenterOfMass()
 
@@ -577,7 +1011,8 @@ func (ss *SpriteSet) DrawSprite() {
 		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT)
 		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
 		gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
-		gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA, int32(width), int32(height), 0, gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(img.Pix))
+
+		gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA, int32(img.Rect.Max.X), int32(img.Rect.Max.Y), 0, gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(img.Pix))
 		gl.Uniform1i(gl.GetUniformLocation(program, gl.Str("ourTexture\x00")), 0)
 		worldTranslate := mgl32.Ident4()
 		gl.UniformMatrix4fv(gl.GetUniformLocation(program, gl.Str("world\x00")), 1, false, &worldTranslate[0])
@@ -588,30 +1023,6 @@ func (ss *SpriteSet) DrawSprite() {
 		glfw.PollEvents()
 		window.SwapBuffers()
 	}
-}
-
-func PerpendicularDistance(pt, lineStart, lineEnd Point) float32 {
-
-	dx := float64(lineEnd.x - lineStart.x)
-	dy := float64(lineEnd.y - lineStart.y)
-	mag := math.Sqrt(math.Pow(dx, 2) + math.Pow(dy, 2))
-	if mag > 0 {
-		dx /= mag
-		dy /= mag
-	}
-
-	pvx := float64(pt.x - lineStart.x)
-	pvy := float64(pt.y - lineStart.y)
-
-	pvdot := dx*pvx + dy*pvy
-
-	dsx := pvdot * dx
-	dsy := pvdot * dy
-
-	ax := pvx - dsx
-	ay := pvy - dsy
-
-	return float32(math.Sqrt(math.Pow(ax, 2) + math.Pow(ay, 2)))
 }
 
 func RamerDouglasPeucker(pointList []Point, epsilon float32) []Point {
--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"image"
+	"image/draw"
 	"image/png"
 	"log"
 	"os"
 	"runtime"
 
-	"github.com/go-gl/gl/v2.1/gl"
+	"github.com/go-gl/gl/v4.1-core/gl"
 	"github.com/go-gl/glfw/v3.3/glfw"
+	"github.com/go-gl/mathgl/mgl32"
+	"github.com/skycoin/cx-game/camera"
+	cv "github.com/skycoin/cx-game/cmd/spritetool"
 	"github.com/skycoin/cx-game/render"
 	"github.com/skycoin/cx-game/world"
 )
@@ -31,17 +35,84 @@ const (
 	height = 480
 )
 
-func main() {
+var (
+	sprite = []float32{
+		1, 1, 0, 1, 0,
+		1, -1, 0, 1, 1,
+		-1, 1, 0, 0, 0,
 
+		1, -1, 0, 1, 1,
+		-1, -1, 0, 0, 1,
+		-1, 1, 0, 0, 0,
+	}
+)
+
+var wx, wy, wz float32
+var Cam camera.Camera
+var tex uint32
+
+func makeVao() uint32 {
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+
+	var vao uint32
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
+	gl.EnableVertexAttribArray(0)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, 4*len(sprite), gl.Ptr(sprite), gl.STATIC_DRAW)
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 5*4, gl.PtrOffset(0))
+	gl.EnableVertexAttribArray(0)
+	gl.VertexAttribPointer(1, 2, gl.FLOAT, false, 5*4, gl.PtrOffset(4*3))
+	gl.EnableVertexAttribArray(1)
+
+	return vao
+}
+
+func keyCallBack(w *glfw.Window, k glfw.Key, s int, a glfw.Action, mk glfw.ModifierKey) {
+	if a == glfw.Press {
+		if k == glfw.KeyEscape {
+			w.SetShouldClose(true)
+		}
+		if k == glfw.KeyW {
+			wy -= 0.5
+		}
+		if k == glfw.KeyS {
+			wy += 0.5
+		}
+		if k == glfw.KeyA {
+			wx += 0.5
+		}
+		if k == glfw.KeyD {
+			wx -= 0.5
+		}
+		if k == glfw.KeyQ {
+			wz += 0.5
+		}
+		if k == glfw.KeyZ {
+			wz -= 0.5
+		}
+	}
+}
+
+func main() {
+	var SS cv.SpriteSet
+	SS.LoadFile("./assets/sprite.png", 250, false)
+	SS.ProcessContours()
+	SS.DrawSprite()
+	wx = 0
+	wy = 0
+	wz = -10
 	win := render.NewWindow(height, width, true)
 	window := win.Window
+	window.SetKeyCallback(keyCallBack)
 	defer glfw.Terminate()
-
+	VAO := makeVao()
 	program := win.Program
-
+	gl.GenTextures(1, &tex)
 	for !window.ShouldClose() {
 		Tick()
-		draw(window, program)
+		redraw(window, program, VAO)
 	}
 }
 
@@ -49,11 +120,12 @@ func Tick() {
 
 }
 
-func draw(window *glfw.Window, program uint32) {
+func redraw(window *glfw.Window, program uint32, VAO uint32) {
+	gl.ClearColor(1, 1, 1, 1)
 	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 	gl.UseProgram(program)
 
-	imgFile, err := os.Open("./assets/Daco_3555790.png")
+	imgFile, err := os.Open("./assets/cat.png")
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -61,28 +133,30 @@ func draw(window *glfw.Window, program uint32) {
 
 	// Decode detexts the type of image as long as its image/<type> is imported
 	im, err := png.Decode(imgFile)
-
-	img := im.(*image.NRGBA)
-
 	if err != nil {
 		log.Fatalln(err)
 	}
-
+	img := image.NewRGBA(im.Bounds())
+	draw.Draw(img, img.Bounds(), im, image.Pt(0, 0), draw.Src)
 	size := img.Rect.Size()
-
-	// flip image: first pixel is lower left corner
-	data := make([]byte, size.X*size.Y*4)
-	lineLen := size.X * 4
-	dest := len(data) - lineLen
-	for src := 0; src < len(img.Pix); src += img.Stride {
-		copy(data[dest:dest+lineLen], img.Pix[src:src+img.Stride])
-		dest -= lineLen
-	}
-
-	gl.TexImage2D(
-		gl.TEXTURE_2D, 0, gl.RGBA, int32(size.X), int32(size.Y),
-		0, gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(data))
-
+	gl.Enable(gl.TEXTURE_2D)
+	gl.Enable(gl.BLEND)
+	gl.Enable(gl.DEPTH_TEST)
+	gl.DepthFunc(gl.LESS)
+	gl.ActiveTexture(gl.TEXTURE0)
+	gl.BindTexture(gl.TEXTURE_2D, tex)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+	gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA, int32(size.X), int32(size.Y), 0, gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(img.Pix))
+	gl.Uniform1i(gl.GetUniformLocation(program, gl.Str("ourTexture\x00")), 0)
+	worldTranslate := mgl32.Translate3D(wx, wy, wz)
+	gl.UniformMatrix4fv(gl.GetUniformLocation(program, gl.Str("world\x00")), 1, false, &worldTranslate[0])
+	projectTransform := mgl32.Perspective(mgl32.DegToRad(45), float32(width)/float32(height), 0.1, 100.0)
+	gl.UniformMatrix4fv(gl.GetUniformLocation(program, gl.Str("projection\x00")), 1, false, &projectTransform[0])
+	gl.BindVertexArray(VAO)
+	gl.DrawArrays(gl.TRIANGLES, 0, 6)
 	glfw.PollEvents()
 	window.SwapBuffers()
 }
