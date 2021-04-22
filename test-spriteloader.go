7ddc1574c49b12264ea444a38f1e675e3feeda16
Ted Brownlow <ted.brownlow@gmail.com>
2021-04-22 12:27:51 -0400

    began drawing tests for spriteloader

--- test-spriteloader.go
+++ test-spriteloader.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"github.com/skycoin/cx-game/spriteloader"
-	//"github.com/skycoin/cx-game/render"
+	"github.com/skycoin/cx-game/render"
+	"github.com/go-gl/glfw/v3.3/glfw"
 	"log"
 )
 
+func keyCallBack(w *glfw.Window, k glfw.Key, s int, a glfw.Action, mk glfw.ModifierKey) {
+	if a == glfw.Press && k == glfw.KeyEscape {
+		w.SetShouldClose(true)
+	}
+}
 
 func main() {
 	log.Print("running test")
+	win := render.NewWindow(640,480,true)
+	window := win.Window
+	window.SetKeyCallback(keyCallBack)
+	defer glfw.Terminate()
 	spriteSheetId := spriteloader.
 		LoadSpriteSheet("./assets/starfield/stars/Starsheet1.png")
 	spriteloader.
@@ -16,4 +26,8 @@ func main() {
 	spriteId := spriteloader.
 		GetSpriteIdByName("star")
 	log.Print(spriteId)
+	for !window.ShouldClose() {
+		//DrawSpriteQuad(
+		glfw.PollEvents()
+	}
 }
