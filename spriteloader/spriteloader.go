2cd2e055743de2d1aa32c605b564818a46c41a95
Ted Brownlow <ted.brownlow@gmail.com>
2021-04-22 12:27:51 -0400

    began drawing tests for spriteloader

--- spriteloader/spriteloader.go
+++ spriteloader/spriteloader.go
@@ -4,12 +4,18 @@ import (
 	"os"
 	"image/png"
 	"image"
+	"github.com/go-gl/gl/v4.1-core/gl"
 	/*
 	"image/draw"
 	"image/png"
 	*/
 	"log"
 )
+
+// call this before loading any spritesheets
+func initSpriteloader() {
+	quadVao = makeQuadVao();
+}
 
 type Spritesheet struct {
 	img image.Image
@@ -73,3 +79,40 @@ func LoadPng(fname string) image.Image {
 
 	return image.NewRGBA(im.Bounds())
 }
+
+// x,y,z,u,v
+var quadVertexAttributes = []float32{
+	1, 1, 0, 1, 0,
+	1, -1, 0, 1, 1,
+	-1, 1, 0, 0, 0,
+
+	1, -1, 0, 1, 1,
+	-1, -1, 0, 0, 1,
+	-1, 1, 0, 0, 0,
+}
+
+var quadVao uint32
+func makeQuadVao() uint32 {
+	var vbo uint32
+	gl.GenBuffers(1,&vbo);
+
+	var vao uint32
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
+	gl.EnableVertexAttribArray(0)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(
+		gl.ARRAY_BUFFER,
+		4*len(quadVertexAttributes),
+		gl.Ptr(quadVertexAttributes),
+		gl.STATIC_DRAW,
+	)
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 5*4, gl.PtrOffset(0))
+	gl.EnableVertexAttribArray(0)
+	gl.VertexAttribPointer(1, 2, gl.FLOAT, false, 5*4, gl.PtrOffset(4*3))
+	gl.EnableVertexAttribArray(1)
+
+	return vao
+}
+
+
