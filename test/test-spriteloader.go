cce91c78fae850f37c230431a19a11b93692b657
SkycoinSynth <54745307+SkycoinSynth@users.noreply.github.com>
2021-04-25 15:22:38 +0800

    Revert "added simple draw map with move hotkeys"

--- test/test-spriteloader.go
+++ test/test-spriteloader.go
@@ -5,15 +5,15 @@ import (
 	"runtime"
 	"time"
 
+	"github.com/skycoin/cx-game/spriteloader"
+	"github.com/skycoin/cx-game/render"
 	"github.com/go-gl/gl/v4.1-core/gl"
 	"github.com/go-gl/glfw/v3.3/glfw"
-	"github.com/skycoin/cx-game/render"
-	"github.com/skycoin/cx-game/spriteloader"
 )
 
 func init() {
 	// This is needed to arrange that main() runs on main thread.
-	// See documentation for functions that are only allowed to be called
+	// See documentation for functions that are only allowed to be called 
 	// from the main thread.
 	runtime.LockOSThread()
 }
@@ -27,12 +27,17 @@ func keyCallBack(w *glfw.Window, k glfw.Key, s int, a glfw.Action, mk glfw.Modif
 func main() {
 	log.Print("running test")
 	log.Print("You should see an orange square rock.")
+<<<<<<< HEAD
 	win := render.NewWindow(800, 800, true)
+=======
+	win := render.NewWindow(640,480,true)
+>>>>>>> 16cb562... Revert "added simple draw map with move hotkeys"
 	window := win.Window
 	window.SetKeyCallback(keyCallBack)
 	defer glfw.Terminate()
 	spriteloader.InitSpriteloader(&win)
 	spriteSheetId := spriteloader.
+<<<<<<< HEAD
 		LoadSpriteSheetByColRow("../assets/blackcat_sprite.png", 13, 4)
 
 	j := 0
@@ -46,6 +51,17 @@ func main() {
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 		time.Sleep(100 * time.Millisecond)
 		spriteloader.DrawSpriteQuad(0, 0, 1, 1, spriteId)
+=======
+		LoadSpriteSheet("./assets/starfield/stars/planets.png")
+	spriteloader.
+		LoadSprite(spriteSheetId, "star", 2,1)
+	spriteId := spriteloader.
+		GetSpriteIdByName("star")
+	for !window.ShouldClose() {
+		gl.ClearColor(1,1,1,1)
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+		spriteloader.DrawSpriteQuad(0,0,2,2,spriteId)
+>>>>>>> 16cb562... Revert "added simple draw map with move hotkeys"
 		glfw.PollEvents()
 		window.SwapBuffers()
 		j++
