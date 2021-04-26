5f49fa2ca861df156d8448f0e171614be0af552b
Ted Brownlow <ted.brownlow@gmail.com>
2021-04-26 11:00:55 -0400

    add layers to planet

---
+++ test/test-world.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"runtime"
+
+	//"github.com/skycoin/cx-game/spriteloader"
+	"github.com/skycoin/cx-game/render"
+	"github.com/skycoin/cx-game/world"
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/glfw/v3.3/glfw"
+)
+
+func init() {
+	// This is needed to arrange that main() runs on main thread.
+	// See documentation for functions that are only allowed to be called 
+	// from the main thread.
+	runtime.LockOSThread()
+}
+
+var earth *world.Planet
+
+func keyCallBack(w *glfw.Window, k glfw.Key, s int, a glfw.Action, mk glfw.ModifierKey) {
+	if a == glfw.Press && k == glfw.KeyEscape {
+		w.SetShouldClose(true)
+	}
+}
+
+func main() {
+	log.Print("running test")
+	log.Print("You should see an planet.")
+	win := render.NewWindow(640,480,true)
+	window := win.Window
+	window.SetKeyCallback(keyCallBack)
+	defer glfw.Terminate()
+	earth = world.NewPlanet(4,4)
+	for !window.ShouldClose() {
+		gl.ClearColor(1,1,1,1)
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+		earth.Draw()
+		glfw.PollEvents()
+		window.SwapBuffers()
+	}
+}
--- world/planet.go
+++ world/planet.go
@@ -1,16 +1,50 @@
 package world
 
+import (
+	"log"
+)
+
+type Layers struct {
+	Background []Tile
+	Mid []Tile
+	Top []Tile
+}
+
 type Planet struct {
 	Width  int32
 	Height int32
-	Tiles  []Tile
+	Layers Layers
 }
 
 func NewPlanet(x, y int32) *Planet {
+	tiles := make([]Tile,x*y)
 	planet := Planet{
 		Width:  x,
 		Height: y,
+		Layers:  Layers{
+			Background: tiles,
+			Mid: tiles,
+			Top: tiles,
+		},
 	}
 
 	return &planet
+}
+
+func (planet *Planet) DrawLayer(tiles []Tile) {
+	for idx,tile := range tiles {
+		y := int32(idx)/planet.Width
+		x := int32(idx)%planet.Width
+		log.Print(x,y)
+		log.Print(tile)
+	}
+}
+
+func (planet *Planet) Draw() {
+	log.Print("drawing background")
+	planet.DrawLayer(planet.Layers.Background)
+	log.Print("drawing mid")
+	planet.DrawLayer(planet.Layers.Mid)
+	log.Print("drawing top")
+	planet.DrawLayer(planet.Layers.Top)
 }
