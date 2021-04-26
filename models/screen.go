3497ff915ba4d33429cd413468201e5b07108f08
Sanzhar <danceontheair@gmail.com>
2021-04-26 19:37:09 +0600

    moved models folder

---
+++ models/screen.go
@@ -0,0 +1,19 @@
+package models
+
+type Screen struct {
+	x      int
+	y      int
+	height int
+	width  int
+}
+
+func NewScreen() *Screen {
+	screen := Screen{
+		x:      0,
+		y:      0,
+		height: 480,
+		width:  800,
+	}
+
+	return &screen
+}
