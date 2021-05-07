dac5572350f10ae8fac2564fb730531d6f8b45c4
yago <ygtassello@gmail.com>
2021-05-07 12:22:23 -0300

    use one uniform matrix for drawing lines

--- render/lines.go
+++ render/lines.go
@@ -9,17 +9,7 @@ import (
 
 var lineProgram uint32
 
-func (window *Window) DrawLine(x0, y0, z0, x1, y1, z1 float32) {
-	var lineArray = []float32{
-		x0, y0, z0,
-		x1, y1, z1,
-	}
-	color := []float32{1.0, 1.0, 1.0}
-
-	window.DrawLines(lineArray, color)
-}
-
-func (window *Window) DrawLines(lineArray []float32, color []float32) {
+func (window *Window) DrawLines(lineArray []float32, color []float32, worldTransform mgl32.Mat4) {
 	// DEBUG: check if the array have the right amount of elements
 	if len(lineArray) < 6 {
 		log.Panicln("line array doesn't enough points to draw a line")
@@ -56,27 +46,18 @@ func (window *Window) DrawLines(lineArray []float32, color []float32) {
 		&color[0],
 	)
 
-	// unforms
-	worldTransform := mgl32.Mat4.Mul4(
-		mgl32.Translate3D(float32(0.0), float32(0.0), -10),
-		mgl32.Scale3D(float32(1.0), float32(1.0), 1),
-	)
-
 	aspect := float32(window.Width) / float32(window.Height)
 	projectTransform := mgl32.Perspective(
 		mgl32.DegToRad(45), aspect, 0.1, 100.0,
 	)
 
-	gl.UniformMatrix4fv(
-		gl.GetUniformLocation(lineProgram, gl.Str("uWorld\x00")),
-		1, false, &worldTransform[0],
-	)
+	projection := projectTransform.Mul4(worldTransform)
+
 	gl.UniformMatrix4fv(
 		gl.GetUniformLocation(lineProgram, gl.Str("uProjection\x00")),
-		1, false, &projectTransform[0],
+		1, false, &projection[0],
 	)
 
 	gl.BindVertexArray(vao)
-	//gl.BindTexture(gl.TEXTURE_2D, 0)
 	gl.DrawArrays(gl.LINES, 0, int32(len(lineArray)/2))
 }
