de5e4dc307072924fbcb1346478d872c8f355d40
Sanzhar <danceontheair@gmail.com>
2021-05-11 03:51:18 +0600

    removed unused files, added comments

--- utility/loadpng.go
+++ utility/loadpng.go
@@ -13,6 +13,9 @@ func LoadPng(filepath string) *image.RGBA {
 	if err != nil {
 		log.Fatal("error opening file")
 	}
+	//close file
+	defer file.Close()
+
 	im, err := png.Decode(file)
 	if err != nil {
 		log.Fatal("error decoding file")
