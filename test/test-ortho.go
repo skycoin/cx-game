89e046c19caa9ea463ab62220261b86eb21ae0fe
Planutim <danceontheair@gmail.com>
2021-05-15 21:42:40 +0000

    ortho

--- test/test-ortho.go
+++ test/test-ortho.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"runtime"
 	"time"
@@ -18,11 +19,11 @@ func init() {
 	runtime.LockOSThread()
 }
 
-func keyCallBack(w *glfw.Window, k glfw.Key, s int, a glfw.Action, mk glfw.ModifierKey) {
-	if a == glfw.Press && k == glfw.KeyEscape {
-		w.SetShouldClose(true)
-	}
-}
+var (
+	xpos, ypos, xwidth, ywidth float32 = 1, 1, 1, 1
+	// xpos, ypos, xwidth, ywidth float32 = 24, 18, 7, 7
+	zoomValue float32 = 0.05
+)
 
 func main() {
 	win := render.NewWindow(600, 800, true)
@@ -31,18 +32,49 @@ func main() {
 	defer glfw.Terminate()
 	spriteloader.InitSpriteloader(&win)
 	spriteSheetId := spriteloader.
-		LoadSpriteSheet("./assets/starfield/stars/planets.png")
+		LoadSpriteSheet("./assets/starfield/stars/galaxy_256x256.png")
 	spriteloader.
-		LoadSprite(spriteSheetId, "star", 2, 1)
+		LoadSprite(spriteSheetId, "star", 6, 5)
 	spriteId := spriteloader.
 		GetSpriteIdByName("star")
 	rand.Seed(time.Now().UnixNano())
 	for !window.ShouldClose() {
-		gl.ClearColor(1, 1, 1, 1)
+		gl.ClearColor(0.1, 0.2, 0.5, 1)
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
-		spriteloader.DrawSpriteQuadOrtho(300, 300, 32, 32, spriteId)
+		spriteloader.DrawSpriteQuadOrtho(xpos, ypos, xwidth, ywidth, spriteId)
+		// spriteloader.DrawSpriteQuadOrtho(24, 18, 7, 7, spriteId)
 		glfw.PollEvents()
 		window.SwapBuffers()
 	}
+}
+
+func keyCallBack(w *glfw.Window, k glfw.Key, s int, a glfw.Action, mk glfw.ModifierKey) {
+	if a == glfw.Press && k == glfw.KeyEscape {
+		w.SetShouldClose(true)
+	}
+	if a == glfw.Press {
+		switch k {
+		case glfw.KeyW:
+			ypos += 1
+		case glfw.KeyS:
+			ypos -= 1
+		case glfw.KeyA:
+			xpos -= 1
+		case glfw.KeyD:
+			xpos += 1
+		}
+	}
+
+	//zoom
+	if k == glfw.KeyQ {
+		xwidth += zoomValue
+		ywidth += zoomValue
+	}
+	if k == glfw.KeyZ {
+		xwidth -= zoomValue
+		ywidth -= zoomValue
+	}
+
+	fmt.Printf("X: %f,     Y:    %f,        Zoom:          %f\n", xpos, ypos, xwidth)
 }
