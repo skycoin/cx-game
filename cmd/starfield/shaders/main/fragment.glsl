bd427ecb3a913bce1b9f5e8b55dd411da6fabba3
Planutim <danceontheair@gmail.com>
2021-05-26 21:30:48 +0000

    changed fragment discard value

--- cmd/starfield/shaders/main/fragment.glsl
+++ cmd/starfield/shaders/main/fragment.glsl
@@ -10,7 +10,7 @@ uniform float intensity;
 void main() {
     vec4 tex2d = texture(ourTexture, vec2(tCoord.x, tCoord.y));
     vec4 tex1d = texture(texture_1d, gradValue);
-    if (tex2d.a < 0.1)
+    if (tex2d.a < 0.5)
         discard;
     vec4 result = mix(tex2d, tex1d, 0.5);
     frag_colour = vec4(result.xyz, intensity);
