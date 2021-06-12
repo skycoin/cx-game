eb2371d10af5ced34eeb51ba471f7cdeae5d063e
Ted Brownlow <ted.brownlow@gmail.com>
2021-06-12 19:09:56 -0400

    document render context

--- render/context.go
+++ render/context.go
@@ -4,24 +4,32 @@ import (
 	"github.com/go-gl/mathgl/mgl32"
 )
 
+// the render context contains information
+// used to transform graphics to the screen
 type Context struct {
-	World      mgl32.Mat4
-	Size       mgl32.Vec2
-	Projection mgl32.Mat4
+	World      mgl32.Mat4 // 4x4 matrix for local => world
+	Size       mgl32.Vec2 // window size for ratio / frustum calculations
+	Projection mgl32.Mat4 // 4x4 projection matrix for world => screen
 }
 
+// push a local transformation onto the render context.
+// can use to build hierarchies.
 func (ctx Context) PushLocal(local mgl32.Mat4) Context {
 	newCtx := ctx
 	newCtx.World = newCtx.World.Mul4(local)
 	return newCtx
 }
 
+// push a view matrix onto the render context.
+// in the result, the projection matrix is really 
+// a projection-view matrix
 func (ctx Context) PushView(view mgl32.Mat4) Context {
 	newCtx := ctx
 	newCtx.Projection = newCtx.Projection.Mul4(view)
 	return newCtx
 }
 
+// compute the model-view-projection matrix for this render context
 func (ctx Context) MVP() mgl32.Mat4 {
 	return ctx.Projection.Mul4(ctx.World)
 }
