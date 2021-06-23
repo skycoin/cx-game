6cf47df6bff15a210673973270b0841011fc9772
Ted Brownlow <ted.brownlow@gmail.com>
2021-06-23 11:52:48 -0400

    place multiple tile types

--- world/generate.go
+++ world/generate.go
@@ -1,23 +1,34 @@
 package world
 
 import (
-	"log"
+	"math/rand"
 
 	perlin "github.com/skycoin/cx-game/procgen"
 	"github.com/skycoin/cx-game/spriteloader"
 )
 
+// TODO shove in .yaml file
+const persistence = 0.5
+const lacunarity = 2
+
+const heightScale = 4
 func (planet *Planet) placeTileOnTop(x int, tile Tile) {
 	y := planet.GetHeight(x) + 1
 	tileIdx := planet.GetTileIndex(x, y)
 	planet.Layers.Top[tileIdx] = tile
 }
 
-const seed = 2
-const persistence = 0.5
-const lacunarity = 2
+func (planet *Planet) placeLayer(tile Tile, scale float32) {
+	perlin := perlin.NewPerlin2D(rand.Int63(), 512, 4, 256)
+	for x:=int32(0); x<planet.Width; x++ {
+		noise := perlin.Noise(float32(x), 0, persistence, lacunarity, 8)
+		height := int((noise+1) * scale)
+		for i:=0; i<height; i++ {
+			planet.placeTileOnTop(int(x),tile)
+		}
+	}
+}
 
-const heightScale = 4
 
 func GeneratePlanet() *Planet {
 	planet := NewPlanet(100, 100)
@@ -27,21 +38,23 @@ func GeneratePlanet() *Planet {
 		LoadSingleSprite("./assets/tile/bedrock.png", "Bedrock")
 	spriteloader.
 		LoadSingleSprite("./assets/tile/stone.png", "Stone")
-	dirtTile := Tile {
+
+	dirt := Tile {
 		TileType: TileTypeNormal,
 		SpriteID: uint32(spriteloader.GetSpriteIdByName("Dirt")),
 	}
-
-	perlin := perlin.NewPerlin2D(seed, 512, 4, 256)
-
-	for x:=int32(0); x<planet.Width; x++ {
-		dirtNoise := perlin.Noise(float32(x), 0, persistence, lacunarity, 8)
-		log.Printf("dirt noise = %v",dirtNoise)
-		dirtHeight := int((dirtNoise+1) * heightScale)
-		for i:=0; i<dirtHeight; i++ {
-			planet.placeTileOnTop(int(x),dirtTile)
-		}
+	stone := Tile {
+		TileType: TileTypeNormal,
+		SpriteID: uint32(spriteloader.GetSpriteIdByName("Stone")),
 	}
+	bedrock := Tile {
+		TileType: TileTypeNormal,
+		SpriteID: uint32(spriteloader.GetSpriteIdByName("Bedrock")),
+	}
+
+	planet.placeLayer(bedrock, 4)
+	planet.placeLayer(stone, 4)
+	planet.placeLayer(dirt, 4)
 	
 	return planet
 }
