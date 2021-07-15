b4771ba2607bb8046b10ab48991748d181f45bf1
Planutim <danceontheair@gmail.com>
2021-07-15 17:08:33 +0600

    restructured agent_draw

--- components/agents/agent_draw/drawhandlerlist.go
+++ components/agents/agent_draw/drawhandlerlist.go
@@ -7,23 +7,8 @@ import (
 
 var DrawHandlerList []func([]*agents.Agent)
 
-func DrawHandler_1(agents []*agents.Agent) {
-	for _, agent := range agents {
-		spriteloader.DrawSpriteQuad(
-			agent.PhysicsState.Pos.X,
-			agent.PhysicsState.Pos.Y,
-			agent.PhysicsState.Size.X,
-			agent.PhysicsState.Size.Y,
-			getSpriteId(agent.AgentType),
-		)
-	}
-}
-
-func DrawHandler_2(agents []*agents.Agent) {
-
-}
-
 func Init() {
+	//todo
 	spriteloader.LoadSingleSprite("./assets/enemies/basic-enemy.png", "basic-agent")
 	RegisterDrawHandler(DrawHandler_1)
 	RegisterDrawHandler(DrawHandler_2)
@@ -43,11 +28,4 @@ func ChangeDrawHandler(id int, newDefinition func([]*agents.Agent)) {
 
 func GetDrawHandler(id int) func([]*agents.Agent) {
 	return DrawHandlerList[id]
-}
-
-func getSpriteId(agentType int) spriteloader.SpriteID {
-	switch agentType {
-	default:
-		return spriteloader.GetSpriteIdByName("basic-agent")
-	}
 }
