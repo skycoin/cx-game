d3d801d720c7a5781a12aa4ccd887a302da919b9
Heru Susanto <herux-amg@gmail.com>
2021-08-08 15:27:36 +0700

    update spiderdrill objects

--- objects/spiderdrill.go
+++ objects/spiderdrill.go
@@ -1,1 +1,24 @@
 package objects
+
+import (
+	"fmt"
+
+	"github.com/skycoin/cx-game/agents"
+	"github.com/skycoin/cx-game/events"
+)
+
+type spiderDrillJumpNotifier struct {
+	agent *agents.Agent
+}
+
+func SpiderDrillInit() {
+	createNotifier := spiderDrillJumpNotifier{
+		agent: nil,
+	}
+
+	events.OnSpiderJump.Register(createNotifier)
+}
+
+func (s spiderDrillJumpNotifier) OnSpiderDrillJump(data events.SpiderEventData) {
+	fmt.Println("Jump: ", data)
+}
