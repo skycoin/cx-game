72db4a68c2f29f24f74ebd99f5b848b0ea9ac2b2
Planutim <danceontheair@gmail.com>
2021-08-16 00:37:42 +0600

    fix view matrix on agent draw

--- components/agents/agent_draw/color.go
+++ components/agents/agent_draw/color.go
@@ -13,14 +13,14 @@ var debugAgentColor = mgl32.Vec4{0, 1, 0, 1}
 func ColorDrawHandler(agents []*agents.Agent, ctx DrawHandlerContext) {
 	for _, agent := range agents {
 		translate := mgl32.Translate3D(
-			agent.PhysicsState.Pos.X-ctx.Camera.X,
-			agent.PhysicsState.Pos.Y-ctx.Camera.Y,
+			agent.PhysicsState.Pos.X,
+			agent.PhysicsState.Pos.Y,
 			0)
 		scale := mgl32.Scale3D(
 			agent.PhysicsState.Size.X,
 			agent.PhysicsState.Size.Y,
 			1)
-		modelView := translate.Mul4(scale)
+		modelView := ctx.Camera.GetViewMatrix().Mul4(translate.Mul4(scale))
 		render.DrawColorQuad(modelView, debugAgentColor)
 
 	}
