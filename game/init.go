d868ec4623b41db56dbe9d979cffbbc28ab5209a
Heru Susanto <heruxi@gmail.com>
2021-08-27 00:09:10 +0700

    first step creating cursor loader

--- game/init.go
+++ game/init.go
@@ -64,6 +64,24 @@ func Init() {
 		render.FixRenderCOCOA(window)
 	}
 
+	// upLeft := image.Point{0, 0}
+	// lowRight := image.Point{16, 16}
+	// img := image.NewRGBA(image.Rectangle{upLeft, lowRight})
+	// cyan := color.RGBA{100, 200, 200, 0xff}
+	// for x := 0; x < 16; x++ {
+	// 	for y := 0; y < 16; y++ {
+	// 		switch {
+	// 		case x < 16/2 && y < 16/2:
+	// 			img.Set(x, y, cyan)
+	// 		case x >= 16/2 && y >= 16/2:
+	// 			img.Set(x, y, color.White)
+	// 		default:
+	// 		}
+	// 	}
+	// }
+	cursor := glfw.CreateCursor(img, 0, 0)
+	window.SetCursor(cursor)
+
 	window.SetMouseButtonCallback(mouseButtonCallback)
 	window.SetScrollCallback(scrollCallback)
 	window.SetCursorPosCallback(cursorPosCallback)
