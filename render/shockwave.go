5cdd7068fc9b3daae60308588ec0a409d4583e67
Planutim <danceontheair@gmail.com>
2021-10-07 02:47:15 +0600

    adjust shockwave

--- render/shockwave.go
+++ render/shockwave.go
@@ -7,12 +7,13 @@ import (
 )
 
 var (
-	shockwave_center    mgl32.Vec2 = mgl32.Vec2{0.5, 0.5}
-	shockwave_force     float32    = 0.2
-	shockwave_size      float32    = 0.3
-	shockwave_thickness float32    = 0
-	shockwave_enabled   bool       = true
-	inverted            bool       = false
+	shockwave_center        mgl32.Vec2 = mgl32.Vec2{0.5, 0.5}
+	shockwave_force         float32    = 0.2
+	shockwave_size          float32    = 0.3
+	shockwave_thickness     float32    = 0
+	shockwave_thickness_gap float32    = 0.03
+	shockwave_enabled       bool       = true
+	inverted                bool       = false
 
 	slider_size          float32 = 0
 	slider_thickness     float32 = 0
@@ -52,10 +53,10 @@ func SetupShockwave() {
 	// EnableShockwave()
 	ScreenShader.SetBool("u_shockwave_enabled", shockwave_enabled)
 	ScreenShader.SetVec2("data.center", &shockwave_center)
-	ScreenShader.SetFloat("data.force", slider_force)
-	ScreenShader.SetFloat("data.size", slider_size)
-	ScreenShader.SetFloat("data.thickness", slider_thickness)
-	ScreenShader.SetFloat("data.thickness_gap", slider_thickness_gap)
+	ScreenShader.SetFloat("data.force", shockwave_force)
+	ScreenShader.SetFloat("data.size", shockwave_size)
+	ScreenShader.SetFloat("data.thickness", shockwave_thickness)
+	ScreenShader.SetFloat("data.thickness_gap", shockwave_thickness_gap)
 
 	ScreenShader.SetBool("inverted", inverted)
 }
@@ -70,6 +71,10 @@ func SetShockwaveForce(force float32) {
 
 func SetShockwaveThickness(thickness float32) {
 	shockwave_thickness = thickness
+}
+
+func SetShockwaveThicknessGap(gap float32) {
+	shockwave_thickness_gap = gap
 }
 
 func SetShockwaveCenter(x, y float32) {
