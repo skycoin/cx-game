a9c33f850636c2a816d7e1af27bd9bbd9e0be3b4
Planutim <danceontheair@gmail.com>
2021-10-07 02:23:30 +0600

    added easing interpolation functions

--- cxmath/cxmath.go
+++ cxmath/cxmath.go
@@ -8,6 +8,15 @@ import (
 	"github.com/skycoin/cx-game/cxmath/math32"
 )
 
+type EASING_TYPE int
+
+const (
+	SMOOTHSTEP EASING_TYPE = iota
+	EASEOUTSINE
+	EASEINOUTSINE
+	EASEOUTQUAD
+)
+
 func ConvertScreenCoordsToWorld(x, y float32, projection mgl32.Mat4) mgl32.Vec2 {
 	homogenousClipCoords := mgl32.Vec4{x, y, -1.0, 1.0}
 	cameraCoords := projection.Inv().Mul4x1(homogenousClipCoords)
@@ -89,15 +98,63 @@ func Lerp(start, target, alpha float32) float32 {
 	return (alpha * target) + ((1 - alpha) * start)
 }
 
-func ClampF(x, min,max float32) float32 {
-	return math32.Clamp(x,min,max)
+//differnt interpolations
+func Interpolate(start, target, alpha float32, method EASING_TYPE) float32 {
+	alpha = math32.Clamp(alpha, 0, 1)
+	switch method {
+	case SMOOTHSTEP:
+		alpha = SmoothStep(0, 1, alpha)
+	case EASEOUTSINE:
+		alpha = EaseOutSine(alpha)
+	case EASEINOUTSINE:
+		alpha = EaseInOutSine(alpha)
+	case EASEOUTQUAD:
+		alpha = EaseOutQuad(alpha)
+	}
+	return Lerp(start, target, alpha)
 }
 
+//https://easings.net/
+func SmoothStep(edge0, edge1, x float32) float32 {
+	x = ClampF((x-edge0)/(edge1-edge0), 0, 1)
+
+	return x * x * (3 - 2*x)
+}
+
+func EaseOutSine(x float32) float32 {
+	result := float32(math.Sin((float64(x) * math.Pi) / 2))
+
+	// fmt.Println(result)
+	return result
+}
+
+func EaseInOutSine(x float32) float32 {
+	result := -1 * float32(math.Cos(math.Pi*float64(x))-1) / 2
+
+	return result
+	//return -(cos(PI * x) - 1) / 2;
+}
+
+func EaseOutQuad(x float32) float32 {
+	return 1 - (1-x)*(1-x)
+}
+
+func ClampF(x, min, max float32) float32 {
+	return math32.Clamp(x, min, max)
+}
+
+// float smoothstep(float edge0, float edge1, float x)
+// {
+//     // Scale, bias and saturate x to 0..1 range
+//     x = clamp((x - edge0) / (edge1 - edge0), 0.0, 1.0);
+//     // Evaluate polynomial
+//     return x*x*(3 - 2 * x);
+// }
 func DegToRad(x float32) float32 {
-	return x*math.Pi/180
+	return x * math.Pi / 180
 }
 
 func TileAt(pos mgl32.Vec2) Vec2i {
-	x,y := RoundVec2(pos)
-	return Vec2i { x,y }
+	x, y := RoundVec2(pos)
+	return Vec2i{x, y}
 }
